b94f79ed8c30e20cd32499aff07922688d3de081
5566b <106473242+5566b@users.noreply.github.com>
2023-06-19 23:35:02 +0300

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter the age");
+var input1 = Convert.ToInt32(Console.ReadLine());
+if (input1 < 18)
+{
+    Console.WriteLine("minor");
+}
+
+else if (input1 >= 18)
+{
+    Console.WriteLine("adult");
+}
