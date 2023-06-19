59c9911b5169235a66000339154c6378d1f14858
5566b <106473242+5566b@users.noreply.github.com>
2023-06-19 23:27:24 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число:");
+        double num1 = double.Parse(Console.ReadLine());
+
+      
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = double.Parse(Console.ReadLine());
+
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+            case "%"
+                result = num1 % num2;
+                Console.WriteLine("Операция не распознана.");
+                break;
+        }
+
+        Console.WriteLine("Результат: " + result);
+
+    }
+}
---
+++ площадь прямоугольного треугольника/ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("enter 1 cathet ");
+
+var input1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter 2 cathet ");
+var input2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("S right triangle ");
+var input3 = (input1 * input2);
+
+Console.WriteLine(input3);
